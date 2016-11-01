7afbc27476af3d55b0dbd7eeb3dbe1924d909d9c
icalF <laser.survivor@gmail.com>
2016-11-01 13:49:19 +0700

    be skeleton

--- _index.js
+++ _index.js
@@ -3,3 +3,37 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var _ = require('lodash');
 var uuid = require('uuid');
+
+io.on('connection', function (client) {
+  // TODO: push queue
+
+  /* User */
+  client.on('register', function (userData) {
+    // body...
+  });
+  client.on('login', function(credential) {
+    // body...
+  });
+  client.on('add_friend', function (uname) {
+    // body...
+  });
+
+  /* Room */
+  client.on('find', function (friendId) {
+    // body...
+  });
+  client.on('create', function (roomData) {
+    // body...
+  });
+  client.on('add', function (memberId) {
+    // body...
+  });
+  client.on('kick', function (memberId) {
+    // body...
+  });
+
+  /* Chat */
+  client.on('send', function (roomId) {
+    // body...
+  });
+});
